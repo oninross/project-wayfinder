9f661859822fc22391ba04f3a2d11675e624642c
Nino Ross Rodriguez <oninross@gmail.com>
2016-08-10 17:01:48 +0800

    Added in pushcrew script for push notifications

--- assets/wayfinder/js/modules.js
+++ assets/wayfinder/js/modules.js
@@ -1,4 +1,4 @@
-/*! wayfinder 2016-08-01 */
+/*! wayfinder 2016-08-10 */
 /* global RR: true, TweenMax: true, TimelineMax: true, jQuery: true, ripple: true, Ease: true, Expo: true */
 /* jshint unused: false */
 
